43fba5d53dd10f459496fa1806c1d45f6e435605
GoogolHz <GoogolHz@users.noreply.github.com>
2023-02-13 09:21:15 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,8 +24,6 @@ dotenv.config();
 function runApp() {
 	// Start listening for connections, and serve static files.
 	const server = new MRE.WebHost({
-		// baseUrl: "https://weasel.mre.ninja",
-		// port: 3002,
 		baseUrl: "`ws://10.0.1.12",
 		port: 3901,
 		baseDir: resolvePath(__dirname, "../public"),
